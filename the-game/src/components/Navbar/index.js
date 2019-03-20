84ebe1ec3f23ec0b9d4f79e2af030c9b97642a30
gurneet33 <gurneet.me@gmail.com>
2019-03-20 18:34:50 -0400

    deploying

--- the-game/src/components/Navbar/index.js
+++ the-game/src/components/Navbar/index.js
@@ -4,11 +4,12 @@ import './style.css'
 function Navbar(props) {
     return (
         <nav className="nav navbar navbar-expand-lg navbar-light bg-light ">
+
+            <h3 id="nav-title" className="navbar-item">Clicky Game</h3>
             <div className="row header">
-                <h3 id="nav-title" className="navbar-item">Clicky Game</h3>
-                <h3 id="nav-guess" className="navbar-item justify-center">{props.guess}</h3>
-                <h3 id="nav-score" className="navbar-item">Score:{props.count}</h3>
-                <h3 id="nav-score" className="navbar-item">Top Score:{props.high}</h3>
+                <h3 id="nav-guess" className="navbar-item">{props.guess}</h3>
+                <h3 id="nav-score" className="navbar-item score">Score:{props.count}</h3>
+                <h3 id="nav-score" className="navbar-item top-score">Top Score:{props.high}</h3>
             </div>
         </nav>
     )
